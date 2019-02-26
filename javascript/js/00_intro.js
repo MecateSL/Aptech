407c610de35582f1bd4ad74b36bdcd91914319de
MecateSL <cristianrg333@gmail.com>
2019-02-26 10:13:32 +0530

    25/02

---
+++ javascript/js/00_intro.js
@@ -0,0 +1,51 @@
+// JavaScript Document
+/*
+	
+	Javascript:
+	
+	It adds interactiviry to your static HTL pages.
+	
+	Javascript is a lightweight programming language. Any programming 
+	language has statement or commands which tells computer or in
+	this case the browser how to reacto or perform a certain action.
+	
+	A typical javascript statement is composed of functions, keywords,
+	variables, comments, etc.
+	
+	Every statement in javascript must close with a semi-colon (;).
+	
+	You cn group statements together using a block { ... }.
+	
+	Javascript is case-sensitive and it uses camel-case, that means if
+	you have two words -> 1st word's 1st letter will be lowercase and
+	subsequent word's first letter is capital.
+	
+	Ex.: 
+		backgroundColor
+		getElementById
+		
+	Ir was invented by Charls Goldfarn in the year 1995. The official
+	name of javascript is ECMA - 262 (European Computer Manufacturers 
+	Association).
+	
+	Javascript can be directly embedded in you HTML pages or you can 
+	create an external javacript file (.js) and can be linged to your
+	HTML pages. External js is very useful when you have to run a code
+	in different HTML pages.
+	
+	When you embed javascript in HTML pages it can be kept in head 
+	section or body section but it is preferred to have all scripts
+	at one place. Mostly we keep it in the body at the end.
+	
+	To create a function you must use te keyword function followed by 
+	any name (apply naming rules) and followed by "()".
+	
+	function myFunction("parameter"){
+		
+		// Statements;
+	}
+	
+	A function performs certain action based on the statements 
+	mentioned inside the block.
+
+ */
